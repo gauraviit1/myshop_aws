b272324a3b2f417ebe9eff32fb44a18bc56cd101
gauraviit1 <mcjail.shi.hp@gmail.com>
2017-01-05 15:57:39 +0530

    temporary changed

--- myshop/settings/production.py
+++ myshop/settings/production.py
@@ -14,7 +14,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 import os
 import dj_database_url
-
+os.environ.get('SECRET_KEY')
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
