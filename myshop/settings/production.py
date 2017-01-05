b85d9c03d5c64b822f42abce3fe04b5d22c00db3
gauraviit1 <mcjail.shi.hp@gmail.com>
2017-01-05 11:27:08 +0530

    deleted base.py

--- myshop/settings/production.py
+++ myshop/settings/production.py
@@ -11,178 +11,177 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-if not settings.DEBUG:
-    import os
-    import dj_database_url
 
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+import os
+import dj_database_url
 
-
-    # Quick-start development settings - unsuitable for production
-    # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
-    # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
-    # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = False
-
-    # Allow all host headers
-    ALLOWED_HOSTS = ['*']
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-    # Application definition
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
-    INSTALLED_APPS = (
-        # 'dal',
-        # 'dal_select2',
-        'django.contrib.admin',
-        'django.contrib.auth',
-        'django.contrib.contenttypes',
-        'django.contrib.sessions',
-        'django.contrib.messages',
-        'django.contrib.staticfiles',
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-        'django.contrib.postgres',
-        'storages',
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
 
-        'crispy_forms',
-        'sorl.thumbnail',
-        'shop',
-        'cart',
-        'orders',
-        'testimonials',
-    )
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
-    MIDDLEWARE_CLASSES = (
-        'django.contrib.sessions.middleware.SessionMiddleware',
-        'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
-        'django.contrib.auth.middleware.AuthenticationMiddleware',
-        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-        'django.contrib.messages.middleware.MessageMiddleware',
-        'django.middleware.clickjacking.XFrameOptionsMiddleware',
-        'django.middleware.security.SecurityMiddleware',
-    )
 
-    ROOT_URLCONF = 'myshop.urls'
+# Application definition
 
-    TEMPLATES = [
-        {
-            'BACKEND': 'django.template.backends.django.DjangoTemplates',
-            'DIRS': [os.path.join(BASE_DIR, 'templates')]
-            ,
-            'APP_DIRS': True,
-            'OPTIONS': {
-                'context_processors': [
-                    'django.template.context_processors.debug',
-                    'django.template.context_processors.request',
-                    'django.contrib.auth.context_processors.auth',
-                    'django.contrib.messages.context_processors.messages',
-                    'cart.context_processors.cart',
-                    'shop.context_processors.bakery_items',
-                    'shop.context_processors.handicraft_items',
-                ],
-            },
+INSTALLED_APPS = (
+    # 'dal',
+    # 'dal_select2',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'django.contrib.postgres',
+    'storages',
+
+    'crispy_forms',
+    'sorl.thumbnail',
+    'shop',
+    'cart',
+    'orders',
+    'testimonials',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+ROOT_URLCONF = 'myshop.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        ,
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
+                'shop.context_processors.bakery_items',
+                'shop.context_processors.handicraft_items',
+            ],
         },
-    ]
+    },
+]
 
-    WSGI_APPLICATION = 'myshop.wsgi.application'
+WSGI_APPLICATION = 'myshop.wsgi.application'
 
 
-    # Database
-    # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 
-    # Internationalization
-    # https://docs.djangoproject.com/en/1.8/topics/i18n/
+# Internationalization
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-    LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-us'
 
-    TIME_ZONE = 'UTC'
+TIME_ZONE = 'UTC'
 
-    USE_I18N = True
+USE_I18N = True
 
-    USE_L10N = True
+USE_L10N = True
 
-    USE_TZ = True
-
-
-
-    # Update database configuration with $DATABASE_URL.
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
-
-    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_TZ = True
 
 
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-    STATIC_URL = '/static/'
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
-    # Extra places for collectstatic to find static files.
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
-
-    # Simplified static file serving.
-    # https://warehouse.python.org/project/whitenoise/
-    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
-    # MEDIA_URL = '/media/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-    MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
-    CART_SESSION_ID = 'cart'
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
-    EMAIL_HOST = 'smtp.gmail.com'
-
-    EMAIL_HOST_USER = 'mcjail.shi.hp@gmail.com'
-
-    EMAIL_HOST_PASSWORD = os.environ.get(' EMAIL_HOST_PASSWORD')
-
-    EMAIL_PORT = 587
-
-    EMAIL_USE_TLS = True
-
-    CRISPY_TEMPLATE_PACK = 'bootstrap3'
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+# MEDIA_URL = '/media/'
 
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 
-    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+CART_SESSION_ID = 'cart'
 
-    AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+EMAIL_HOST = 'smtp.gmail.com'
 
-    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+EMAIL_HOST_USER = 'mcjail.shi.hp@gmail.com'
 
-    AWS_S3_HOST = 's3-ap-south-1.amazonaws.com'
+EMAIL_HOST_PASSWORD = os.environ.get(' EMAIL_HOST_PASSWORD')
 
-    os.environ['S3_USE_SIGV4'] = 'True'
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 
-    STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/' + 'static/'
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 
-    # DO NOT DO THIS!
-    MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
-    # STATIC_URL = 'http://s3.amazonaws.com/'  + AWS_STORAGE_BUCKET_NAME +"/"
-    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-    TEMPLATE_DEBUG = False
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_S3_HOST = 's3-ap-south-1.amazonaws.com'
+
+os.environ['S3_USE_SIGV4'] = 'True'
+
+
+STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/' + 'static/'
+
+# DO NOT DO THIS!
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+# STATIC_URL = 'http://s3.amazonaws.com/'  + AWS_STORAGE_BUCKET_NAME +"/"
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
