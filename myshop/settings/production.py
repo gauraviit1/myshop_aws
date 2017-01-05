0a0ae42b4742249cc1680ce0fc6a143d17120c60
gauraviit1 <mcjail.shi.hp@gmail.com>
2017-01-05 16:17:47 +0530

    adapted for prod.py

--- myshop/settings/production.py
+++ myshop/settings/production.py
@@ -108,7 +108,7 @@ DATABASES = {
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
-        'POST': '',
+        'PORT': '',
     }
 }
 
@@ -119,13 +119,7 @@ import urlparse
 urlparse.uses_netloc.append("postgres")
 url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-conn = psycopg2.connect(
-    database=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-)
+
 
 DATABASES['default'] =  dj_database_url.config()
 
