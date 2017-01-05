6a3b605a7f7558dc65c0bda0257aa06599874441
gauraviit1 <mcjail.shi.hp@gmail.com>
2017-01-05 15:55:15 +0530

    changed secret key

--- myshop/settings/production.py
+++ myshop/settings/production.py
@@ -23,7 +23,7 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'dsgdsgdfgdfhfghngfh7etdhdj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
