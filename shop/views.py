734d42d2de3a10a8c6e8e4f750b386f314e8b5f1
unknown <mcjail.shi.hp@gmail.com>
2017-01-22 14:09:15 +0530

    changed view for parent ad child product

--- shop/views.py
+++ shop/views.py
@@ -1,44 +1,50 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from shop.models import Cateogry, Product
 from cart.forms import CartAddProductForm
 from testimonials.models import Testimonial_
 
 
 # Create your views here.
+def mainPage(request):
+    cateogries = Cateogry.objects.all()
+    first_testimonial = Testimonial_.objects.first()
+    testimonials = Testimonial_.objects.all()[:5]
+    return render(request, 'shop/product/main.html', {'cateogries': cateogries,
+                                                      'testimonials': testimonials,
+                                                      'first_testimonial': first_testimonial, })
+
+
 def product_list(request, cateogry_slug=None):
     cateogry = None
     cateogries = Cateogry.objects.all()
     testimonials = Testimonial_.objects.all()[:5]
     products = Product.objects.filter(available=True)
     if cateogry_slug:
-        cateogry = get_object_or_404(Cateogry,slug=cateogry_slug)
+        cateogry = get_object_or_404(Cateogry, slug=cateogry_slug)
         products = products.filter(cateogry=cateogry)
     return render(request, 'shop/product/list.html', {
         'cateogry': cateogry,
         'cateogries': cateogries,
         'products': products,
-        'testimonials':testimonials,
+        'testimonials': testimonials,
     })
 
 
 def product_detail(request, id, slug):
 
     product = get_object_or_404(Product, id=id,
-                                     slug=slug,
-                                     available=True)
+                                slug=slug,
+                                available=True)
+    child_products = Product.objects.filter(parent_product=product)
+
+    if child_products:
+        return redirect(child_products[0])
+
+    else:
+        pass
+
     cart_product_form = CartAddProductForm()
     return render(request, 'shop/product/detail.html',
                   {'product': product,
                    'cart_product_form': cart_product_form,
                    })
-
-
-def mainPage(request):
-    cateogries = Cateogry.objects.all()
-    first_testimonial = Testimonial_.objects.first()
-    testimonials = Testimonial_.objects.all()[:5]
-    return render(request, 'shop/product/main.html', {'cateogries': cateogries, 
-                                                       'testimonials': testimonials,
-                                                       'first_testimonial': first_testimonial,} )
-
-
