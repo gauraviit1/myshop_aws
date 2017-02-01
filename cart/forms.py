ef79117ebabe7079c82adb07f2652c0215ab93b8
gauraviit1 <mcjail.shi.hp@gmail.com>
2017-02-01 16:17:05 +0530

    changed more details font size -- smaller

--- cart/forms.py
+++ cart/forms.py
@@ -1,8 +1,20 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
+    # def __init__(self, *args, **kwargs):
+        # self.helper = FormHelper()
+        # self.helper.layout = Layout(
+        #     Fieldset(
+        #     ),
+        #     ButtonHolder(
+        #         Submit('submit', 'Submit', css_class='button white')
+        #     )
+        # )
+        # super(ExampleForm, self).__init__(*args, **kwargs)
     quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,
                                       coerce=int)
 
