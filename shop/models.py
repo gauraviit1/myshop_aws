ddc8438dbae1a6c9199f0fccd1e38ab111e281d4
gauraviit1 <mcjail.shi.hp@gmail.com>
2017-02-15 14:30:55 +0530

    added optionality display on more detail page

--- shop/models.py
+++ shop/models.py
@@ -122,13 +122,13 @@ class ModifiedProduct(MPTTModel):
                             related_name='children', db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     description = models.TextField(blank=True)
-    description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
     stock = models.PositiveIntegerField()
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     features = JSONField(blank=True, null=True)
+    option_type = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
         return self.name
@@ -186,16 +186,20 @@ class ModifiedProduct(MPTTModel):
     def get_option_name(self):
         try:
             pattern = '(?<=\()(.*?)(?=\))'
-            return re.search(pattern, self.name).group()
+            return re.findall(pattern, self.name)[-1]
         except:
             pass
 
     def name_for_hierarchial_list(self):
         try:
             pattern = '(?<=\()(.*?)(?=\))'
-            return re.search(pattern, self.name).group()
+            return re.findall(pattern, self.name)[-1]
         except:
             return self.name
+
+    def get_options(self):
+        options = self.get_ancestors().exclude(level=0)
+        return options
 
 
 class ProductImages(models.Model):
