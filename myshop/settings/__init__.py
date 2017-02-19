0de7bf96d6413ad0578beae229eec588cced5425
gauraviit1 <mcjail.shi.hp@gmail.com>
2017-02-19 14:17:33 +0530

    added amazon rds

--- myshop/settings/__init__.py
+++ myshop/settings/__init__.py
@@ -97,17 +97,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myshop.wsgi.application'
 
-if live:
-	DATABASES = {
-  'default': {
-      'ENGINE': 'django.db.backends.postgresql_psycopg2',
-      'NAME': '',
-      'USER': '',
-      'PASSWORD': '',
-      'HOST': '',
-      'PORT': '',
-  }
-}
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
 else:
 
 	DATABASES = {
