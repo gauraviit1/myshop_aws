6f9299ff6d759eae34ccc2c4b800f1ff4841f0fa
gauraviit1 <mcjail.shi.hp@gmail.com>
2017-02-26 07:03:11 +0530

    fixed ordetitem model

--- orders/models.py
+++ orders/models.py
@@ -25,7 +25,7 @@ class Order(models.Model):
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items')
-    product = models.ForeignKey(ModifiedProduct, related_name='order_items', default=5)
+    product = models.ForeignKey(ModifiedProduct, related_name='order_items')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
 
