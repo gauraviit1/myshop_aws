261b296d79cfdf8fa4cb9105b4e2fe70e864f6a6
gauraviit1 <mcjail.shi.hp@gmail.com>
2017-03-03 13:22:58 +0530

    fixed testimonial sidebar

--- testimonials/views.py
+++ testimonials/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from testimonials.models import Testimonial_
 from testimonials.forms import TestimonialForm
+from django.http import JsonResponse, HttpResponse
+from django.core import serializers
+import json
+
 
 def read_and_write_testimonial(request):
     testimonials = Testimonial_.objects.all()
@@ -9,9 +13,14 @@ def read_and_write_testimonial(request):
     	if form.is_valid():
     		form.save()
     else:
-    	form = TestimonialForm()		
+    	form = TestimonialForm()
     return render(request, 'testimonial/testimonial.html',
                   {'form': form,
                    'testimonials': testimonials})
 
 
+def selected_testimonials(request):
+    testimonials = Testimonial_.objects.all().values('name', 'comment')[:5]
+    # data = serializers.serialize("json", testimonials)
+    data = json.dumps(list(testimonials))
+    return HttpResponse(data, content_type='application/json')
